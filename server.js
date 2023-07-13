1f59cd43413333ee92bb0bf24626d1f87920c7c5
AlexDuncan2100 <63152765+AlexDuncan2100@users.noreply.github.com>
2023-07-13 11:31:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get('/api/logs', function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log('Server is running on port 3000');
+app.listen(80, function() {
+  console.log('Server is running on port 80');
 });
